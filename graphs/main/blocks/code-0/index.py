894ae22a3afee082f9949d23113ed44e59caf28f
TaoZeyu <i@taozeyu.com>
2024-02-02 10:58:21 +0800

    refactor(main): use Github API

--- graphs/main/blocks/code-0/index.py
+++ graphs/main/blocks/code-0/index.py
@@ -1,2 +1,25 @@
+import os
+import re
+import requests
+
+def token():
+  file_path = os.path.abspath(os.path.join(__file__, "../../../../token.txt"))
+  with open(file_path, "r") as file:
+    content = file.read()
+    content = re.sub(r"[\s\n]+", "", content)
+  return content
+
 def main(props, context):
-  context.result(props, "out", True)
+  response = requests.get(
+    "https://api.github.com/repos/flows-example/github-project-migrator/projects", 
+    headers={
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"Bearer {token()}",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  )
+  if response.status_code != 200:
+    print(response.content)
+    raise Exception(f"status {response.status_code}")
+  
+  context.result(response.json(), "out", True)
